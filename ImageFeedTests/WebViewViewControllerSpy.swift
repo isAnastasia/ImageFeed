979b929a8e17cc7c551d03f85c715cc83afc5f7e
isAnastasia <nasty04012003@gmail.com>
2024-02-07 17:05:03 +0300

    WebView tests

---
+++ ImageFeedTests/WebViewViewControllerSpy.swift
@@ -0,0 +1,20 @@
+import Foundation
+import ImageFeed
+
+final class WebViewViewControllerSpy: WebViewViewControllerProtocol {
+    var presenter: ImageFeed.WebViewPresenterProtocol?
+    var loadCalled: Bool = false
+    
+    func load(request: URLRequest) {
+        loadCalled = true
+    }
+    
+    func setProgressValue(_ newValue: Float) {
+        
+    }
+    
+    func setProgressHidden(_ isHidden: Bool) {
+        
+    }
+    
+}
